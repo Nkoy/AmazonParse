9dcef6e89bfa381d9258e2239c8bd353304a7aee
Nkem Dockery <kard91@yahoo.com>
2011-03-29 18:44:55 -0400

    Nkem test

--- amazonparse.py
+++ amazonparse.py
@@ -10,21 +10,11 @@
     -Avg Review
     -Number of reviews
     -Items the customer also bought (w/ links)
-    -Items frequently bought w/book (w/likes)
     -Format
-    -Pages
-    -Language
     -Publisher
-    -Edition
-    -Date Published
-    -ISBN (10/13)
-    -Product Dimensions
-    -Shipping weight
-    -Subjects
 """
 from __future__ import print_function
 from lxml import html
-from lxml import etree
 import re
 
 
@@ -34,8 +24,15 @@ class Parser(object):
     easier access.
     """
     def __init__(self, in_line):
-        self.source = html.parse(in_line).getroot()
-        self.productDetails = self.getProductDetails()
+        if in_line.find("http://") == -1:
+            f_ile = open(in_line).readlines()
+            link = ""
+            for line in f_ile:
+                link = link + line
+        else:
+            link = in_line
+
+        self.source = html.parse(link).getroot()
 
     @property
     def title(self):
@@ -43,7 +40,7 @@ class Parser(object):
             groups. author, format and title etc. are children of the same
             parent"""
         res = self.source.get_element_by_id("btAsinTitle").text
-        return "Title: " + res.strip()
+        return res.strip()
 
     @property
     def author(self):
@@ -53,8 +50,8 @@ class Parser(object):
         res = self.source.get_element_by_id("btAsinTitle").getparent() \
               .getparent()
         res = res.iterlinks()
-        res_list = [link[0].text_content() for link in res]
-        fmt_res = "Author(s): " + ", ".join(res_list)
+        res_list = [link[0].text for link in res]
+        fmt_res = ", ".join(res_list)
         return fmt_res
 
     @property
@@ -65,170 +62,80 @@ class Parser(object):
         res = self.source.get_element_by_id("btAsinTitle")[0].text_content()
         return res.strip("[]")
 
-    def getProductDetails(self):
-        """
-        Create a list containing the product details
-        """
-        tmpList = []
-        for elem in self.source.xpath(".//h2"):
-            if elem.text == 'Product Details':
-                currNode = elem.getparent()
-                for li in currNode.xpath(".//li"):
-                    tmpList.append(li)
-                return tmpList   
-        return("None")
+    def getPrices(self):
+        """"""
+        res = self.source.get_element_by_id("priceBlock") \
+              .xpath(".//tr")
+
+        price_list = []
+        for row in res:
+            if len(row) == 2:
+                price_list.append(row.text_content().strip().split(":"))
+
+        price_list = [i.strip().partition("\n")[0] for elem in price_list for i in elem]
+
+        
+        def pair(test):
+            if test:
+                yield (test.pop(), test.pop())
+
+        price_list.reverse()
+        res = [tup for tup in pair(price_list)]
+        return res
 
     @property
-    def language(self):
-        """
-        Use the Product details list to
-        find the language
-        """
-        for elem in self.productDetails:
-            if( elem.text_content().find("Language") != -1):
-                return(elem.text_content().split(':')[1].strip()) 
-        return("None")
+    def listPrice(self):
+        res = self.getPrices()
+        if self.type_ == "Kindle":
+            label = "Digital List Price"
+        else:
+            label = "List Price"
+            
+        for elem in res:
+            if elem[0].find(label) != -1:
+                return elem[1].replace("$", "")
+    @property
+    def crrntPrice(self):
+        res = self.getPrices()
+        if self.type_ == "Kindle":
+            label = "Kindle Price"
+        else:
+            label = "Price"
+            
+        for elem in res:
+            if elem[0].find(label) != -1:
+                return elem[1].replace("$", "")
 
     @property
-    def pages(self):
-        """
-        Uses the Product Details list to
-        find the pages
-        """
-        for elem in self.productDetails:
-            if( elem.text_content().find("page") != -1):
-                return(elem.text_content().split(':')[1].split(' ')[1].strip()) 
-        return("None")
+    def numReviews(self):
+        res = self.source.find_class("crAvgStars")
+        for elem in res:
+            check = re.search(r'(?P<reviews>\d+) customer reviews',
+                              elem.text_content())
+            if check:
+                return str(check.group('reviews'))
 
-
-    @property
-    def edition(self):
-        """
-        Use the Product Details list to
-        find the book's edition
-        """
-        for elem in self.productDetails:
-#edition is under published info, find this li first
-#cannot search on edition or will pick up kindle
-            if( elem.text_content().find("Publisher") != -1):
-                edLoc = elem.text_content().find("ed")
-                if( edLoc != -1 ):
-                    semiLoc = elem.text_content()[:edLoc].find(';')
-                    return(elem.text_content()[semiLoc+1:edLoc].strip())
-        return("None")
-
-
-    @property
-    def date(self):
-        """
-        Use the Product Details list to
-        find the book's edition
-        """
-        for elem in self.productDetails:
-#date is under published info, find this li first
-            if( elem.text_content().find("Publisher") != -1):
-                openLoc = elem.text_content().find("(")
-                if( openLoc != -1 ):
-                    closeLoc = elem.text_content().find(')')
-                    return(elem.text_content()[openLoc+1:closeLoc].strip())
-        return("None")
-
-
-    @property
-    def isbn10(self):
-        """
-        Use the Product Details to
-        find the book's ISBN10
-        NOTE*****: most kindle seem to have
-        an ASIN which is 10, should I make that
-        the kindle isbn10????
-        """
-        for elem in self.productDetails:
-            if(elem.text_content().find("ISBN-10") != -1):
-                return(elem.text_content().split(':')[1].strip())
-        return("None")
-
-
-    @property
-    def isbn13(self):
-        """
-        Use the Product Details to
-        find the book's ISBN13
-        """
-        for elem in self.productDetails:
-            if(elem.text_content().find("ISBN-13") != -1):
-                return(elem.text_content().split(':')[1].strip())
-        return("None")
-
+        return None
     
     @property
-    def publisher(self):
-        """
-        Use the Prodcut Details to
-        find the book's publisher
-        """
-        for elem in self.productDetails:
-            if(elem.text_content().find("Publisher") != -1):
-#If there is an endtion the published will come bfore the ';'
-                if(elem.text_content().find(';') != -1):
-                    return(elem.text_content().split(':')[1][:elem.text_content().split(':')[1].find(';')].strip())
-#other wise it comes before (date)
-                elif(elem.text_content().find('(') != -1):
-                    return(elem.text_content().split(':')[1][:elem.text_content().split(':')[1].find('(')].strip())
-                else:
-                    return("None")
-        return("None")
+    def avgReview(self):
+        res = self.source.find_class("crAvgStars")
+        for elem in res:
+            check = re.search(r'(?P<avg>\d.\d) out of 5',
+                              elem.text_content())
+            if check:
+                return str(check.group('avg'))
 
+        return None
 
     @property
-    def dimensions(self):
-        """
-        Use the Product Details to
-        find the book's dimentions
-        """
-#kindle books do not have dimensions,
-#if its kindle dont waste time
-        if(self.type_ != 'Kindle Edition'): 
-            for elem in self.productDetails:
-                if(elem.text_content().find("Product Dimensions") != -1):
-                    return(elem.text_content().split(':')[1].strip())
-            return("None")
-        return("None")
-
-
-    @property 
-    def weight(self):
-        """
-        Use the product details to
-        find the book's weight
-        """
-#kindle books do not have weight,
-#if its kindle dont waste time
-        if(self.type_ != 'Kindle Edition'): 
-            for elem in self.productDetails:
-                if(elem.text_content().find("Shipping Weight") != -1):
-                    return(elem.text_content().split(':')[1][:elem.text_content().split(':')[1].find('(')].strip())
-            return("None")
-        return("None")
-
-
-    @property
-    def subjects(self):
-        """
-        Parse for the book's subjects
-        """
-        subsList = []
-        for elem in self.source.xpath(".//h2"):
-            if elem.text == "Look for Similar Items by Subject":
-                currNode = elem.getparent()
-                for li in currNode.xpath(".//label"):
-                    subsList.append(li.text_content())
-                return subsList
-        return("None")
-
-
-
-
+    def alsoBought(self):
+        res = self.source.get_element_by_id("purchaseButtonWrapper") \
+            .xpath(".//ul")
+        links = res[0].iterlinks()
+        res = [elem[2] for elem in links
+               if elem[0].get("title") != None]
+        return " \n ".join(res)
 
 if __name__ == "__main__":
     import doctest
@@ -247,18 +154,3 @@ if __name__ == "__main__":
 ##
 ##select count(type) from sqlite_master where type='table' and
 ##    name='TABLE_NAME_TO_CHECK';
-
-
-#create the re to find ed or edition
-#"""
-#pattern = re.compile(r'''
-#                (?<init> \s*)
-#                (?P<publisher> \w+)
-#                (?<edition> \d)
-#                [(]
-#                (?<date> \w)
-#                [)]
-#                ''', re.MULTILINE | re.DOTALL | re.VERBOSE)
-#temp = pattern.search(elem.text_content())
-#print(temp.group('edition'))
-#"""
